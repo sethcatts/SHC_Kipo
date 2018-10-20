4d2434f975f0bd7fed25e1126da77e9125c1a12d
thefarwolf <seth.h.catts@gmail.com>
2018-10-19 23:16:24 -0400

    Finishing keywork checking function for now

--- helpers/keywordCheck.js
+++ helpers/keywordCheck.js
@@ -2,9 +2,28 @@ const tools = require("./componentFunctions.js");
 module.exports = {
    //ARGS: messsage[object]
    //Send a specified text or image base on keyword use.
+   //This implementation is truly awful, fix later
    keywordCheck: function(message) {
       contentArray = message.content.split(" ");
-      console.log(contentArray);
-      //not sure how to implement this without it being awful, ill come back
+      if(contentArray.includes("weeb") || contentArray.includes("weaboo")) {
+         message.channel.send("Did someone say weeb?");
+         tools.sendImage(message, "./images/otaku.png");
+      } else if(contentArray.includes("fuck")
+             || contentArray.includes("shit")
+             || contentArray.includes("damn")) {
+         tools.sendImage(message, "./images/noswearing.jpg");
+      } else if(contentArray.includes("why")
+             || contentArray.includes("stop")
+             || contentArray.includes("stupid")) {
+         tools.sendImage(message, "./images/dudetrustme.png");
+      } else if(contentArray.includes("kipo")
+             || contentArray.includes("bot")
+             || contentArray.includes("robot")) {
+         tools.sendImage(message, "./images/watcher.jpg");
+      } else if(contentArray.includes("fun")
+             || contentArray.includes("lol")
+             || contentArray.includes("lmao")) {
+         tools.sendImage(message, "./images/funpolice.jpg");
+      }
    }
 }
