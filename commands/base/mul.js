846feb6b544e88b067a64f457de9dbb6e695c655
thefarwolf <seth.h.catts@gmail.com>
2018-10-19 23:28:19 -0400

    Adding more math commands

---
+++ commands/base/mul.js
@@ -0,0 +1,17 @@
+const commando = require('discord.js-commando');
+class add extends commando.Command {
+   constructor(client) {
+      super(client, {
+         name: 'mul',
+         group: 'base',
+         memberName: 'mul',
+         description: 'return the product of the argument'
+      });
+   }
+   async run(message, args) {
+      var str = args.split("*");
+      var ans = parseInt(str[0]) * parseInt(str[1]);
+      message.channel.send("Answer: " + ans);
+   }
+}
+module.exports = add;
