0b5a7eca4f82a580c5c00871dfef2e664cce5b51
thefarwolf <seth.h.catts@gmail.com>
2018-10-21 19:13:25 -0400

    Adding comments

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const Commando     = require('discord.js-commando');
 const path         = require("path");
 const keywordCheck = require("./helpers/keywordCheck.js");
 
+//Init client
 const client = new Commando.Client({
    owner: botconfig.ownerID,
    commandPrefix: botconfig.prefix
 });
 
+//On login status update
 client.on('ready', () => {
    console.log("Kipo status: ONLINE");
 })
 
+//Listen and run keyword check function
 client.on('message', (message) => {
    console.log("Running keyword check");
    keywordCheck.keywordCheck(message);
 })
 
+//Register command groups
 client.registry
    .registerGroups([
       ["base", "base commands"]
